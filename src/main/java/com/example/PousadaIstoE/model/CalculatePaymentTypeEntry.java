a7f739068bc0886eafb4f5a28c81805537d5f4ae
SAM <sam105666@ceuma.com.br>
2024-02-28 20:33:16 -0300

    adicionado sintomas da pessoa

---
+++ src/main/java/com/example/PousadaIstoE/model/CalculatePaymentTypeEntry.java
@@ -0,0 +1,55 @@
+package com.example.PousadaIstoE.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "a05_calculate_payment_type")
+public class CalculatePaymentTypeOvernight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "a05_id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "a05_id_payment_type")
+    private PaymentType paymentType;
+
+    @Column(name = "a05_value")
+    private Float value;
+
+    @ManyToOne
+    @JoinColumn(name = "a05_id_overnight")
+    private OvernightStay overnightStay;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PaymentType getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(PaymentType paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+
+    public OvernightStay getOvernightStay() {
+        return overnightStay;
+    }
+
+    public void setOvernightStay(OvernightStay overnightStay) {
+        this.overnightStay = overnightStay;
+    }
+}
---
+++ src/main/java/com/example/PousadaIstoE/repository/CalculatePaymentTypeEntryRepository.java
@@ -0,0 +1,4 @@
+package com.example.PousadaIstoE.repository;
+
+public interface CalculatePaymentTypeEntryRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.PousadaIstoE.model.CalculatePaymentTypeEntry, java.lang.Long> {
+}
